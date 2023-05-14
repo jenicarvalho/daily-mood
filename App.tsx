0d57a54681b7879991defe08f11a6a8264fd7a06
Jeniffer Carvalho <jenicarvalho@live.com>
2023-05-13 23:26:06 -0300

    created initial screen

--- App.tsx
+++ App.tsx
@@ -1,20 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { ThemeProvider } from 'styled-components/native';
+import React from 'react';
+
+import Home from '@screens/Home';
+import { Text } from 'react-native';
+import theme from './src/theme';
+import { useFonts, Nunito_400Regular, Nunito_700Bold } from '@expo-google-fonts/nunito';
 
 export default function App() {
+
+  const [loaded] = useFonts({ Nunito_400Regular, Nunito_700Bold })
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ThemeProvider theme={theme}>
+      <StatusBar
+        style="auto"
+        backgroundColor="transparent"
+        translucent
+      />
+      {loaded ? <Home /> : <Text>Loading...</Text>}
+    </ThemeProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
